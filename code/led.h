64151f582f5c539a6cc74611b7dd3d63c73826ea
Yahia Elghoniemy <yahiaamr90@gmail.com>
2024-10-20 15:53:59 +0300

    def guard

--- code/led.h
+++ code/led.h
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+#if !defined(R) || !defined(G) || !defined(B)
 #define R 0
 #define G 1
 #define B 2
+#endif
 
 void init();
 void set_color(int r, int g, int b);
